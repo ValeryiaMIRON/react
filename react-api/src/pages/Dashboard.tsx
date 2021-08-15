cc12f82fddaa468802b1407be1c63a139880e201
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-08-15 15:18:18 +0300

    feat: add state in main page Dashboard

--- react-api/src/pages/Dashboard.tsx
+++ react-api/src/pages/Dashboard.tsx
@@ -2,16 +2,25 @@
 import React, { useState } from 'react';
 import SearchBar from '../components/searchBar/SearchBar';
 import Card from '../components/card/Card';
-import { Article } from '../types/types';
-// import Posts from './Posts';
+import { Article, SortType } from '../types/types';
+import Sort from '../components/sort/Sort';
 
 const Dashboard = () => {
   const [state, setState] = useState<Article[]>([]);
-  // console.log(state);
-
+  const [sortBy, setSortBy] = useState<SortType>(SortType.relevancy);
+  const [from, setFrom] = useState('');
+  const [to, setTo] = useState('');
   return (
     <div className="dashboard">
-      <SearchBar setState={setState} />
+      <SearchBar setState={setState} sortBy={sortBy} from={from} to={to} />
+      <Sort
+        setSortBy={setSortBy}
+        sortBy={sortBy}
+        setFrom={setFrom}
+        from={from}
+        to={to}
+        setTo={setTo}
+      />
       {state.map((card, index) => {
         return <Card key={index.toString()} card={card} />;
       })}
