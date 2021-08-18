3773ee2ec7621778213540a273b8295d4180ddf9
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-08-18 18:13:18 +0300

    feat: add button for pagination and styles

--- react-api/src/components/paginataion/Pagination.tsx
+++ react-api/src/components/paginataion/Pagination.tsx
@@ -1,56 +1,56 @@
-import React, { ChangeEvent, FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
+
 import { paginationProps } from '../../types/types';
 import './Pagination.scss';
 
-const Pagination: FC<paginationProps> = ({ page, onChangePage, pageSize }) => {
-  const [pagPage, setPagPage] = useState<number | string>('');
+const Pagination: FC<paginationProps> = ({ page, pageSize, paginate, articles }) => {
+  // const [pagPage, setPagPage] = useState<number | string>('');
 
   const pageNumber = [];
 
-  for (let i = 1; i <= Math.ceil(pageSize); i = +1) {
+  for (let i = 1; i <= Math.ceil(100 / pageSize); i += 1) {
     pageNumber.push(i);
   }
 
-  useEffect(() => {
-    setPagPage(page);
-  }, [page]);
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    const regExp = /\d+/;
-    const matchedValue = value.match(regExp);
-    if (matchedValue) {
-      const newValue = +matchedValue[0];
-      onChangePage(newValue);
-      setPagPage(newValue);
-    } else {
-      setPagPage('');
-    }
-  };
+  // useEffect(() => {
+  //   setPagPage(page);
+  // }, [page]);
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   const { value } = e.target;
+  //   const regExp = /\d+/;
+  //   const matchedValue = value.match(regExp);
+  //   if (matchedValue) {
+  //     const newValue = +matchedValue[0];
+  //     onChangePage(newValue);
+  //     setPagPage(newValue);
+  //   } else {
+  //     setPagPage('');
+  //   }
+  // };
 
   return (
     <div className="pagination">
-      <div>
-        <label htmlFor="pagination">
-          <input type="text" name="pagination" value={pagPage} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div>
-        <ul>
-          {/* {pageNumber.map((number) => {
-          return (
-            <li key={number}>
-              <a href="!#">{number}</a>
-            </li>
-          );
-        })} */}
-          {pageNumber.map((number) => (
-            <li key={number}>
-              <a href="!#">{number}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {articles.length ? (
+        <div>
+          {/* <div>
+            <label htmlFor="pagination">
+              <input type="text" name="pagination" value={pagPage} onChange={handleChange} />
+            </label>
+          </div> */}
+          <div className="pagination-pageNumber">
+            {pageNumber.map((number) => (
+              <button
+                type="button"
+                className={page === number ? 'selectedPage' : 'null'}
+                key={number}
+                onClick={() => paginate(number)}
+              >
+                {number}
+              </button>
+            ))}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
