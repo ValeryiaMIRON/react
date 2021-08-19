37abfdb8675e2d417dc64e6006d81dd68c390e20
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-08-19 18:47:09 +0300

    feat: add second page About

---
+++ react-router/src/pages/About.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../components/header/Header';
+import './About.scss';
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <div className="about-container">
+        {' '}
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus itaque velit ex possimus
+        in illum reprehenderit dolore soluta nihil tenetur laborum quasi cum provident, quidem
+        aliquam blanditiis tempora. Dolorem, obcaecati? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Sed minima odit esse libero, voluptates mollitia, voluptatum vel tempora
+        ex rem accusamus at voluptatem magnam similique dolorum quasi dolorem animi quibusdam. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Corrupti sequi corporis unde ipsum? Sit
+        quibusdam nulla minima quia a perferendis. Eius, quia tempora voluptatum possimus neque
+        consequatur et debitis. Accusamus.
+      </div>
+    </div>
+  );
+};
+
+export default About;
