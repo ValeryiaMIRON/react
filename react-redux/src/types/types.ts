a952d69f3b1b604b0376d8601675b3bf9f0f5123
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-09-14 16:11:49 +0300

    feat: delete extra types

--- react-redux/src/types/types.ts
+++ react-redux/src/types/types.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 export interface Article {
   author: string;
   content: string;
@@ -15,35 +14,12 @@ export interface CardProps {
   card: Article;
 }
 
-export interface GET200_Articles {
-  articles: Article[];
-  // totalResults: number;
-}
-
-export interface Props {
-  // setSearchData: (searchData: string) => void;
-  // paginate: (pageNumber: number) => void;
-}
+// export interface GET200_Articles {
+//   articles: Article[];
+// }
 
 export enum SortType {
   relevancy = 'relevancy',
   popularity = 'popularity',
   publishedAt = 'publishedAt',
-}
-
-export interface SortProps {
-  sortBy: SortType;
-  setSortBy: (sortBy: SortType) => void;
-  from: string;
-  setFrom: (from: string) => void;
-  to: string;
-  setTo: (to: string) => void;
-}
-export interface paginationProps {
-  page: number;
-  onChangePage: (pageFromInput: number) => void;
-  pageSize: number;
-  // paginate: (pageNumber: number) => void;
-  articles: Article[];
-  setPageSize: (pageSize: number) => void;
 }
